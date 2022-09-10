db45e52048e62f73dd13071cb72608ed2b7ed3b5
Mihaly Papp <misi@pappmihaly.com>
2022-09-10 16:40:19 +0200

    chore: add wrap-git-project lib

---
+++ packages/wrap-git-project/README.md
@@ -0,0 +1,11 @@
+# wrap-git-project
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test wrap-git-project` to execute the unit tests via [Jest](https://jestjs.io).
+
+## Running lint
+
+Run `nx lint wrap-git-project` to execute the lint via [ESLint](https://eslint.org/).
---
+++ packages/wrap-git-project/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'wrap-git-project',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/wrap-git-project',
+}
