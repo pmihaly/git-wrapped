012d30a842b9e6cd41ca2bd51f6d3d1e9166db20
Mihaly Papp <misi@pappmihaly.com>
2022-09-12 13:27:42 +0200

    refactor: point-free wrapGitRepo

--- packages/wrap-git-project/src/lib/wrap-git-repo.ts
+++ packages/wrap-git-project/src/lib/wrap-git-repo.ts
@@ -1,6 +1,6 @@
 import * as RA from 'fp-ts/ReadonlyArray'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
-import { apply, pipe } from 'fp-ts/function'
+import { apply, flow } from 'fp-ts/function'
 
 import { CreateStatisticFrom, GitRepo, Statistic } from '.'
 
@@ -10,5 +10,5 @@ export type WrapGitRepo = (
   s: RNEA.ReadonlyNonEmptyArray<CreateStatisticFrom<GitRepo>>
 ) => ReadonlyArray<Statistic>
 
-export const wrapGitRepo: WrapGitRepo = (repo) => (stats) =>
-  pipe(stats, RNEA.map(apply(repo)), RA.compact)
+export const wrapGitRepo: WrapGitRepo = (repo) =>
+  flow(RNEA.map(apply(repo)), RA.compact)
