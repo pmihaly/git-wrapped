9c3d285612a3598466e18b5b77c3e65abb219056
Mihaly Papp <misi@pappmihaly.com>
2022-09-12 13:40:22 +0200

    feat: add optional description

--- packages/wrap-git-project/src/lib/statistics/statistic.ts
+++ packages/wrap-git-project/src/lib/statistics/statistic.ts
@@ -7,12 +7,14 @@ import { Chart, GitRepo, createFakeChart } from '..'
 export type Statistic = {
   name: NES.NonEmptyString
   headline: NES.NonEmptyString
+  description: O.Option<NES.NonEmptyString>
   charts: ReadonlyArray<Chart>
 }
 
 export const createFakeStatistic = (s: Partial<Statistic>): Statistic => ({
   name: NES.unsafeFromString('Test statistic'),
   headline: NES.unsafeFromString('Test statistic with **a value**'),
+  description: O.some(NES.unsafeFromString('Test description')),
   charts: [createFakeChart({})],
   ...s,
 })
