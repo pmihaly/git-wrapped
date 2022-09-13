ab652e246f9db3f9e3105466bebe3a930827be00
Mihaly Papp <misi@pappmihaly.com>
2022-09-13 19:38:21 +0200

    refactor: simplify RNEA creation

--- packages/wrap-git-project/src/lib/wrap-git-repo.spec.ts
+++ packages/wrap-git-project/src/lib/wrap-git-repo.spec.ts
@@ -1,12 +1,12 @@
 import * as NES from 'fp-ts-std/NonEmptyString'
 import * as O from 'fp-ts/Option'
-import * as RA from 'fp-ts/ReadonlyArray'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
-import { constant, pipe } from 'fp-ts/function'
+import { constant } from 'fp-ts/function'
 
 import {
   CreateStatisticFrom,
   GitRepo,
+  Statistic,
   createFakeGitRepo,
   createFakeStatistic,
   createFakeStatisticCreator,
@@ -16,59 +16,53 @@ import {
 describe('wrapGitRepo', () => {
   it('should create statistics for a git repo', () => {
     const repo = createFakeGitRepo({})
-    const stats = pipe(
+    const stats: RNEA.ReadonlyNonEmptyArray<CreateStatisticFrom<GitRepo>> = [
       createFakeStatisticCreator({
         name: NES.unsafeFromString('Statistic 1'),
         headline: NES.unsafeFromString('Test statistic 1'),
       }),
-      RNEA.of,
-      RA.append(
-        createFakeStatisticCreator({
-          name: NES.unsafeFromString('Statistic 2'),
-          headline: NES.unsafeFromString('Test statistic 2'),
-        })
-      )
-    )
+      createFakeStatisticCreator({
+        name: NES.unsafeFromString('Statistic 2'),
+        headline: NES.unsafeFromString('Test statistic 2'),
+      }),
+    ]
 
     const res = wrapGitRepo(repo)(stats)
 
-    const expectedResults = pipe(
+    const expectedResults: RNEA.ReadonlyNonEmptyArray<Statistic> = [
       createFakeStatistic({
         name: NES.unsafeFromString('Statistic 1'),
         headline: NES.unsafeFromString('Test statistic 1'),
       }),
-      RNEA.of,
-      RA.append(
-        createFakeStatistic({
-          name: NES.unsafeFromString('Statistic 2'),
-          headline: NES.unsafeFromString('Test statistic 2'),
-        })
-      )
-    )
+      createFakeStatistic({
+        name: NES.unsafeFromString('Statistic 2'),
+        headline: NES.unsafeFromString('Test statistic 2'),
+      }),
+    ]
 
     expect(res).toStrictEqual(expectedResults)
   })
 
   it('should omit none statistics', () => {
     const repo = createFakeGitRepo({})
-    const createNoneStatistic: CreateStatisticFrom<GitRepo> = constant(O.none)
-    const stats = pipe(
+    const noneStatisticCreator = constant(O.none)
+    const stats: RNEA.ReadonlyNonEmptyArray<CreateStatisticFrom<GitRepo>> = [
       createFakeStatisticCreator({
         name: NES.unsafeFromString('Statistic 1'),
         headline: NES.unsafeFromString('Test statistic 1'),
       }),
-      RNEA.of,
-      RA.append(createNoneStatistic)
-    )
+      noneStatisticCreator,
+    ]
 
     const res = wrapGitRepo(repo)(stats)
 
-    const expectedResults = RA.of(
+    const expectedResults = [
       createFakeStatistic({
         name: NES.unsafeFromString('Statistic 1'),
         headline: NES.unsafeFromString('Test statistic 1'),
-      })
-    )
+      }),
+    ]
+
     expect(res).toStrictEqual(expectedResults)
   })
 })
