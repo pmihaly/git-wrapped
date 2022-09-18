365717b543ba38a677362509a3fca0a35a46d073
Mihaly Papp <misi@pappmihaly.com>
2022-09-18 16:04:11 +0200

    feat: add chart type customization

--- libs/present-wrapped-project-ui/src/statistic/fun-fact/chart/chart.spec.tsx
+++ libs/present-wrapped-project-ui/src/statistic/fun-fact/chart/chart.spec.tsx
@@ -1,11 +1,12 @@
 import { createFakeChart } from '@git-wrapped/wrap-git-project'
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 
 import { Chart } from '.'
 
 const MockChartJs: any = ({ type, data }: any) => (
   <>
-    <p id="type">{type}</p>
+    <p aria-label="type">{type}</p>
     <ul>
       {data.labels.map((l: string, i: number) => (
         <li key={`label-${i}`} aria-label="label">
@@ -17,6 +18,22 @@ const MockChartJs: any = ({ type, data }: any) => (
 )
 
 describe('Chart', () => {
+  it('should display a chart of given type', () => {
+    const chart = createFakeChart({ type: 'radar' })
+
+    render(<Chart chartJs={MockChartJs} chart={chart} />)
+
+    expect(screen.getByLabelText('type')).toHaveTextContent('radar')
+  })
+
+  it('should convert area type to line', () => {
+    const chart = createFakeChart({ type: 'area' })
+
+    render(<Chart chartJs={MockChartJs} chart={chart} />)
+
+    expect(screen.getByLabelText('type')).toHaveTextContent('line')
+  })
+
   it('should display labels of a chart', () => {
     const chart = createFakeChart({})
 
--- libs/present-wrapped-project-ui/src/statistic/fun-fact/chart/chart.tsx
+++ libs/present-wrapped-project-ui/src/statistic/fun-fact/chart/chart.tsx
@@ -1,12 +1,18 @@
-import { Chart as ChartModel } from '@git-wrapped/wrap-git-project'
+import { Chart as ChartModel, ChartType } from '@git-wrapped/wrap-git-project'
 import { TextProps } from '@nextui-org/react'
+import { ChartType as ChartJsType } from 'chart.js'
 import * as NES from 'fp-ts-std/NonEmptyString'
 import * as RA from 'fp-ts/ReadonlyArray'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import { pipe } from 'fp-ts/lib/function'
 import { Chart as ReactChartJs } from 'react-chartjs-2'
 
+const toChartJsType: (c: ChartType) => ChartJsType = (c) => (c === 'area' ? 'line' : c)
+
 export type ChartProps = { chartJs: typeof ReactChartJs; chart: ChartModel; labelsProps?: TextProps }
 export const Chart: React.FC<ChartProps> = ({ chartJs: ChartJs, chart }) => (
-  <ChartJs type="bar" data={{ labels: pipe(chart.labels, RNEA.map(NES.toString), RA.toArray), datasets: [] }} />
+  <ChartJs
+    type={toChartJsType(chart.type)}
+    data={{ labels: pipe(chart.labels, RNEA.map(NES.toString), RA.toArray), datasets: [] }}
+  />
 )
