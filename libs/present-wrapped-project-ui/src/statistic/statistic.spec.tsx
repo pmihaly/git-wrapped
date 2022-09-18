59a11954be9a719b9bfeb1d66d1eafd39da631e4
Mihaly Papp <misi@pappmihaly.com>
2022-09-18 08:57:45 +0200

    feat: display statistic texts

--- libs/present-wrapped-project-ui/src/statistic/statistic.spec.tsx
+++ libs/present-wrapped-project-ui/src/statistic/statistic.spec.tsx
@@ -2,13 +2,15 @@ import { createFakeStatistic } from '@git-wrapped/wrap-git-project'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import * as NES from 'fp-ts-std/NonEmptyString'
-import { flow } from 'fp-ts/lib/function'
+import * as O from 'fp-ts/Option'
+import { constant, flow } from 'fp-ts/lib/function'
 import * as S from 'fp-ts/string'
 
 import { Statistic } from '.'
 import { createFakeTheme } from '../theme'
 
 const fromMarkdownNES = flow(NES.toString, S.replace(/\*\*/g, ''))
+const fromOptionMarkdownNES = flow(O.getOrElse(constant(NES.unsafeFromString('no text defined'))), fromMarkdownNES)
 
 describe('Statistic', () => {
   it('should display the heading', () => {
@@ -23,5 +25,33 @@ describe('Statistic', () => {
     )
 
     expect(screen.getByRole('presentation')).toHaveTextContent(fromMarkdownNES(statistic.headline))
+  })
+
+  it('should display the text if given', () => {
+    const statistic = createFakeStatistic({ text: O.some(NES.unsafeFromString('test text')) })
+
+    render(
+      <Statistic
+        theme={createFakeTheme({})}
+        statistic={statistic}
+        textProps={{ normalTextProps: { role: 'presentation' } }}
+      />
+    )
+
+    expect(screen.getByRole('presentation')).toHaveTextContent(fromOptionMarkdownNES(statistic.text))
+  })
+
+  it('should not display the text if not given', () => {
+    const statistic = createFakeStatistic({ text: O.none })
+
+    render(
+      <Statistic
+        theme={createFakeTheme({})}
+        statistic={statistic}
+        textProps={{ normalTextProps: { role: 'presentation' } }}
+      />
+    )
+
+    expect(screen.queryByRole('presentation')).toBeNull()
   })
 })
--- libs/present-wrapped-project-ui/src/statistic/statistic.tsx
+++ libs/present-wrapped-project-ui/src/statistic/statistic.tsx
@@ -1,5 +1,7 @@
 import { Statistic as StatisticModel } from '@git-wrapped/wrap-git-project'
 import * as NES from 'fp-ts-std/NonEmptyString'
+import * as O from 'fp-ts/Option'
+import { constant, pipe } from 'fp-ts/function'
 
 import { Theme } from '..'
 import { GradientMarkdownText, GradientMarkdownTextProps } from '../typography'
@@ -8,12 +10,26 @@ export type StatisticProps = {
   statistic: StatisticModel
   theme: Theme
   headlineProps?: Omit<Partial<GradientMarkdownTextProps>, 'children'>
+  textProps?: Omit<Partial<GradientMarkdownTextProps>, 'children'>
 }
-export const Statistic: React.FC<StatisticProps> = ({ headlineProps, theme, statistic }) => (
-  <GradientMarkdownText
-    normalTextProps={{ h1: true }}
-    textGradient={NES.toString(theme.textGradient)}
-    {...headlineProps}
-    children={NES.toString(statistic.headline)}
-  />
+export const Statistic: React.FC<StatisticProps> = ({ headlineProps, textProps, theme, statistic }) => (
+  <>
+    <GradientMarkdownText
+      normalTextProps={{ h1: true }}
+      textGradient={NES.toString(theme.textGradient)}
+      {...headlineProps}
+      children={NES.toString(statistic.headline)}
+    />
+
+    {pipe(
+      statistic.text,
+      O.match(constant(null), (text) => (
+        <GradientMarkdownText
+          textGradient={NES.toString(theme.textGradient)}
+          {...textProps}
+          children={NES.toString(text)}
+        />
+      ))
+    )}
+  </>
 )
