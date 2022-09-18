77a6df6af6633660df6342cc3771d1047c5c4f6e
Mihaly Papp <misi@pappmihaly.com>
2022-09-18 19:48:49 +0200

    refactor: break component into memoized jsx

--- libs/present-wrapped-project-ui/src/statistic/statistic/statistic.tsx
+++ libs/present-wrapped-project-ui/src/statistic/statistic/statistic.tsx
@@ -3,6 +3,7 @@ import * as NES from 'fp-ts-std/NonEmptyString'
 import * as O from 'fp-ts/Option'
 import * as RNEA from 'fp-ts/ReadonlyNonEmptyArray'
 import { constant, pipe } from 'fp-ts/function'
+import { useMemo } from 'react'
 
 import { FunFact, FunFactProps } from '..'
 import { GradientMarkdownText, GradientMarkdownTextProps, Theme } from '../..'
@@ -14,31 +15,50 @@ export type StatisticProps = {
   textProps?: Omit<Partial<GradientMarkdownTextProps>, 'children'>
   funFactsProps?: Partial<FunFactProps>
 }
-export const Statistic: React.FC<StatisticProps> = ({ funFactsProps, headlineProps, textProps, theme, statistic }) => (
-  <>
-    <GradientMarkdownText
-      normalTextProps={{ h1: true }}
-      textGradient={NES.toString(theme.textGradient)}
-      {...headlineProps}
-      children={NES.toString(statistic.headline)}
-    />
+export const Statistic: React.FC<StatisticProps> = ({ funFactsProps, headlineProps, textProps, theme, statistic }) => {
+  const headline = useMemo(
+    () => (
+      <GradientMarkdownText
+        normalTextProps={{ h1: true }}
+        textGradient={NES.toString(theme.textGradient)}
+        {...headlineProps}
+        children={NES.toString(statistic.headline)}
+      />
+    ),
+    [theme.textGradient, headlineProps, statistic.headline]
+  )
 
-    {pipe(
-      statistic.text,
-      O.match(constant(null), (text) => (
-        <GradientMarkdownText
-          textGradient={NES.toString(theme.textGradient)}
-          {...textProps}
-          children={NES.toString(text)}
-        />
-      ))
-    )}
+  const text = useMemo(
+    () =>
+      pipe(
+        statistic.text,
+        O.match(constant(null), (text) => (
+          <GradientMarkdownText
+            textGradient={NES.toString(theme.textGradient)}
+            {...textProps}
+            children={NES.toString(text)}
+          />
+        ))
+      ),
+    [statistic.text, theme.textGradient, textProps]
+  )
 
-    {pipe(
-      statistic.funFacts,
-      RNEA.mapWithIndex((i, funFact) => (
-        <FunFact key={`fun-fact-${i}`} theme={theme} funFact={funFact} {...funFactsProps} />
-      ))
-    )}
-  </>
-)
+  const funFacts = useMemo(
+    () =>
+      pipe(
+        statistic.funFacts,
+        RNEA.mapWithIndex((i, funFact) => (
+          <FunFact key={`fun-fact-${i}`} theme={theme} funFact={funFact} {...funFactsProps} />
+        ))
+      ),
+    [statistic.funFacts, theme, funFactsProps]
+  )
+
+  return (
+    <>
+      {headline}
+      {text}
+      {funFacts}
+    </>
+  )
+}
