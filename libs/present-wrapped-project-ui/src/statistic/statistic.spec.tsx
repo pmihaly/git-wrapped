732d4fc662e0aee29c5cfda0d28105ed49391f6d
Mihaly Papp <misi@pappmihaly.com>
2022-09-18 09:03:52 +0200

    refactor: organize test cases into describe

--- libs/present-wrapped-project-ui/src/statistic/statistic.spec.tsx
+++ libs/present-wrapped-project-ui/src/statistic/statistic.spec.tsx
@@ -13,45 +13,48 @@ const fromMarkdownNES = flow(NES.toString, S.replace(/\*\*/g, ''))
 const fromOptionMarkdownNES = flow(O.getOrElse(constant(NES.unsafeFromString('no text defined'))), fromMarkdownNES)
 
 describe('Statistic', () => {
-  it('should display the heading', () => {
-    const statistic = createFakeStatistic({})
+  describe('heading', () => {
+    it('should display the heading', () => {
+      const statistic = createFakeStatistic({})
 
-    render(
-      <Statistic
-        theme={createFakeTheme({})}
-        statistic={statistic}
-        headlineProps={{ normalTextProps: { role: 'presentation' } }}
-      />
-    )
+      render(
+        <Statistic
+          theme={createFakeTheme({})}
+          statistic={statistic}
+          headlineProps={{ normalTextProps: { role: 'presentation' } }}
+        />
+      )
 
-    expect(screen.getByRole('presentation')).toHaveTextContent(fromMarkdownNES(statistic.headline))
+      expect(screen.getByRole('presentation')).toHaveTextContent(fromMarkdownNES(statistic.headline))
+    })
   })
 
-  it('should display the text if given', () => {
-    const statistic = createFakeStatistic({ text: O.some(NES.unsafeFromString('test text')) })
+  describe('text', () => {
+    it('should display the text if given', () => {
+      const statistic = createFakeStatistic({ text: O.some(NES.unsafeFromString('test text')) })
 
-    render(
-      <Statistic
-        theme={createFakeTheme({})}
-        statistic={statistic}
-        textProps={{ normalTextProps: { role: 'presentation' } }}
-      />
-    )
+      render(
+        <Statistic
+          theme={createFakeTheme({})}
+          statistic={statistic}
+          textProps={{ normalTextProps: { role: 'presentation' } }}
+        />
+      )
 
-    expect(screen.getByRole('presentation')).toHaveTextContent(fromOptionMarkdownNES(statistic.text))
-  })
+      expect(screen.getByRole('presentation')).toHaveTextContent(fromOptionMarkdownNES(statistic.text))
+    })
+    it('should not display the text if not given', () => {
+      const statistic = createFakeStatistic({ text: O.none })
 
-  it('should not display the text if not given', () => {
-    const statistic = createFakeStatistic({ text: O.none })
+      render(
+        <Statistic
+          theme={createFakeTheme({})}
+          statistic={statistic}
+          textProps={{ normalTextProps: { role: 'presentation' } }}
+        />
+      )
 
-    render(
-      <Statistic
-        theme={createFakeTheme({})}
-        statistic={statistic}
-        textProps={{ normalTextProps: { role: 'presentation' } }}
-      />
-    )
-
-    expect(screen.queryByRole('presentation')).toBeNull()
+      expect(screen.queryByRole('presentation')).toBeNull()
+    })
   })
 })
